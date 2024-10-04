e0435d73565976dc830734fba2e0dc7748fb9d22
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-10-05 00:00:06 +0700

    adjust: packages

--- src/types/index.ts
+++
@@ -1,6 +0,0 @@
-import type { LANGS } from "@/internationalization";
-import type { en } from "@/internationalization/dictionaries/en";
-
-export type Lang = (typeof LANGS)[number];
-export type LangTarget = Lang | null | undefined | string;
-export type Dictionary = typeof en;
