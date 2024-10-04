0a80160747c6844ffdefa5eea8e81cdd336281e6
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-10-04 23:30:28 +0700

    adjust: layout

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -17,8 +17,11 @@ export const formatPhoneNumber = templateFormatter("x (xxx) xxx xx xx");
 export const getCurrentDate = () => {
   const date = new Date();
   const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are zero-based
+  const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
 
   return `${year}-${month}-${day}`;
 };
+
+export const LOCALE = "ru-RU";
+export const formatCurrency = (amount: number) => new Intl.NumberFormat(LOCALE, { style: "currency", currency: "RUR" }).format(amount);
